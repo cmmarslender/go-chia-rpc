6d34560b71951c9fd7d05172189981d867206dcc
Chris Marslender <chrismarslender@gmail.com>
2021-05-21 16:48:50 -0700

    Comment out the parts of the struct that should be uint128 for now

--- pkg/rpc/fullnode.go
+++ pkg/rpc/fullnode.go
@@ -16,7 +16,7 @@ type BlockchainState struct {
 	GenesisChallengeInitialized bool        `json:"genesis_challenge_initialized"`
 	MempoolSize                 uint64      `json:"mempool_size"`
 	Peak                        BlockRecord `json:"peak"`
-	Space                       int64       `json:"space"` // @TODO this is supposed to be uint128 - will run out of room in uint64 pretty soon..
+//	Space                       uint64       `json:"space"` // @TODO this is supposed to be uint128 - will run out of room in uint64 pretty soon..
 	SubSlotIters                uint64      `json:"sub_slot_iters"`
 	Sync                        Sync        `json:"sync"`
 }
@@ -46,8 +46,8 @@ type BlockRecord struct {
 	//SubEpochSummaryIncluded
 	SubSlotIters uint64 `json:"sub_slot_iters"`
 	Timestamp    uint64 `json:"timestamp"`
-	TotalIters   uint64 `json:"total_iters"` // @TODO this is supposed to be uint128
-	Weight       uint64 `json:"weight"`      // @TODO this is supposed to be uint128
+//	TotalIters   uint64 `json:"total_iters"` // @TODO this is supposed to be uint128
+//	Weight       uint64 `json:"weight"`      // @TODO this is supposed to be uint128
 }
 
 // Coin is a coin // @TODO
