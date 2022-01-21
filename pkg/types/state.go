7254ba0a796eddc094ace958801c8d4c9a0b3802
Chris Marslender <chrismarslender@gmail.com>
2022-01-21 15:48:34 -0600

    Add mempool_cost

--- pkg/types/state.go
+++ pkg/types/state.go
@@ -5,6 +5,7 @@ type BlockchainState struct {
 	Difficulty                  uint64       `json:"difficulty"`
 	GenesisChallengeInitialized bool         `json:"genesis_challenge_initialized"`
 	MempoolSize                 uint64       `json:"mempool_size"`
+	MempoolCost                 uint64       `json:"mempool_cost"`
 	Peak                        *BlockRecord `json:"peak"`
 	Space                       Uint128      `json:"space"`
 	SubSlotIters                uint64       `json:"sub_slot_iters"`
