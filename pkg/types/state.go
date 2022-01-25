75e5e5c608a273d908f37550cd4e2fc66e9089a8
Chris Marslender <chrismarslender@gmail.com>
2022-01-25 08:59:35 -0600

    Update struct for mempool min fees

--- pkg/types/state.go
+++ pkg/types/state.go
@@ -7,18 +7,23 @@ type WebsocketBlockchainState struct {
 
 // BlockchainState blockchain state
 type BlockchainState struct {
-	Difficulty                  uint64       `json:"difficulty"`
-	GenesisChallengeInitialized bool         `json:"genesis_challenge_initialized"`
-	MempoolSize                 uint64       `json:"mempool_size"`
-	MempoolCost                 uint64       `json:"mempool_cost"`
-	MempoolMinFee               uint64       `json:"mempool_min_fee"`
-	Peak                        *BlockRecord `json:"peak"`
-	Space                       Uint128      `json:"space"`
-	SubSlotIters                uint64       `json:"sub_slot_iters"`
-	Sync                        *Sync        `json:"sync"`
-	CompactBlocks               uint32       `json:"compact_blocks"`
-	UncompactBlocks             uint32       `json:"uncompact_blocks"`
-	HintCount                   uint64       `json:"hint_count"`
+	Difficulty                  uint64          `json:"difficulty"`
+	GenesisChallengeInitialized bool            `json:"genesis_challenge_initialized"`
+	MempoolSize                 uint64          `json:"mempool_size"`
+	MempoolCost                 uint64          `json:"mempool_cost"`
+	MempoolMinFee               *MempoolMinFees `json:"mempool_min_fee"`
+	Peak                        *BlockRecord    `json:"peak"`
+	Space                       Uint128         `json:"space"`
+	SubSlotIters                uint64          `json:"sub_slot_iters"`
+	Sync                        *Sync           `json:"sync"`
+	CompactBlocks               uint32          `json:"compact_blocks"`
+	UncompactBlocks             uint32          `json:"uncompact_blocks"`
+	HintCount                   uint64          `json:"hint_count"`
+}
+
+// MempoolMinFees minimum fees to get in the mempool at varying costs
+type MempoolMinFees struct {
+	Cost5m uint64 `json:"5000000"`
 }
 
 // Sync struct within blockchain state
